467a7ed82ca5daf251678a939bdd03bb7861e54d
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-16 16:20:13 -0400

    created methods for findCourseById and save in course service and created endpoint for adding new course

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.school.service;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.repository.CourseRepository;
 import com.lambdaschool.school.view.CountStudentsInCourses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class CourseServiceImpl implements CourseService
     }
 
     @Override
+    public Course findCourseById(long id) {
+        return courserepos.findById(id).orElseThrow(() -> new EntityNotFoundException("Course Not Found"));
+    }
+
+    @Override
     public ArrayList<CountStudentsInCourses> getCountStudentsInCourse()
     {
         return courserepos.getCountStudentsInCourse();
@@ -42,5 +48,24 @@ public class CourseServiceImpl implements CourseService
         {
             throw new EntityNotFoundException(Long.toString(id));
         }
+    }
+
+    @Transactional
+    @Override
+    public Course save(Course course) {
+        Course newCourse = new Course();
+
+        newCourse.setCoursename(course.getCoursename());
+        newCourse.setInstructor(course.getInstructor());
+
+        ArrayList<Student> newStudents = new ArrayList<>();
+
+        for(Student s : course.getStudents()){
+            newStudents.add(s);
+        }
+
+        newCourse.setStudents(newStudents);
+
+        return courserepos.save(newCourse);
     }
 }
